bcea5532f5ebba98e118d0ad22ec62463d2583f1
Mlodovico <murilo.lodovico@outlook.com>
2020-02-11 15:49:45 -0300

    1.2.4 alteracao de bd

---
+++ src/database/migrations/20200210234017-recipients.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id:{
+        autoIncrement: true,
+        primarykey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      street: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      complement: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      postal_code: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('recipients');
+  }
+};
